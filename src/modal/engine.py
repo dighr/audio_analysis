ce08bc021e204791d5c77cc7a1ce7932120a30b1
aminjaj <aljailaniamin@yahoo.com>
2018-11-02 12:38:06 -0700

    Added the modal folder, the engine file, and the engine_test file

---
+++ src/modal/engine.py
@@ -0,0 +1,8 @@
+
+class Engine:
+    def __init__(self, name="Engine"):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
---
+++ tests/test_engine.py
@@ -0,0 +1,14 @@
+import unittest
+from src.modal.engine import Engine
+
+
+class EngineTestCases(unittest.TestCase):
+    """Tests for `engine.py`."""
+    def test_engine_default_creation(self):
+        """Is five successfully determined to be prime?"""
+        engine = Engine()
+        self.assertEqual(engine.get_name(), "Engine")
+
+
+if __name__ == '__main__':
+    unittest.main()
