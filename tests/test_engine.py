14e523cb9f8542ca5a6a45005e41ee918851ccdb
aminjaj <aljailaniamin@yahoo.com>
2018-11-07 14:00:27 -0800

    Created a testcase which checks if the application supports unicade charaters #2

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import unittest
 from src.modal.engine import Engine
 
@@ -5,7 +6,7 @@ from src.modal.engine import Engine
 class EngineTestCases(unittest.TestCase):
     """Tests for `engine.py`."""
     def test_singleton(self):
-        """Is five successfully determined to be prime?"""
+        """Assert that not exception is raised"""
         with self.assertRaises(Exception) as context:
             # Trigger an exception if singleton is violated
             Engine()
@@ -13,6 +14,22 @@ class EngineTestCases(unittest.TestCase):
 
         self.assertTrue("This class is a singleton! Call the instance methods"
                         in context.exception)
+
+    def test_unicode(self):
+        """Assert that no exception is made with texts with many unicode characters"""
+        engine = Engine.get_instance()
+        text = " I went to the super market and I was not hÀppÝ with yÝ experience¡. " \
+               "امين افضل واحد" \
+               "The line was super lârge and it wasted a lot of my time that I could have spent ∫¶ ∫¶ ∫¶ ∫¶" \
+               " doing something else ÿŒ Œ Œ"
+        #Check if the test with unicode is supported
+        try:
+            engine.get_text_sentiment_values(text)
+            # Extreme case where the entire sentence is in UNICode characters
+            text = "Åë Òé évˉ ïØ™ ïÒw Yb:@"
+            engine.get_text_sentiment_values(text)
+        except:
+            self.fail("get_text_sentiment_values(text='%s') raised Exception unexpectedly!" % text)
 
 
 if __name__ == '__main__':
