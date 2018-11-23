bf6d62899decbc629ba7779cbd4ff07988866f12
Ameen <aljailaniamin@gmail.com>
2018-11-23 11:12:41 -0600

    Added a testcase to check if one of the requests goes through

--- text_analysis/tests.py
+++ text_analysis/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class ResponseTestCase(TestCase):
+    def test_text_analysis_path_retreives_correct_code(self):
+        c = Client()
+        response = c.get('/text_analysis/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_key_retrieves_correct_values(self):
+        pass
