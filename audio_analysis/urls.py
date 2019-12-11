792615948b7c79d8dd53451e8f0b2a43b4bcce2f
mdrafiurYU <mdrafiur@my.yorku.ca>
2019-12-10 22:05:27 -0500

    Update urls.py

--- audio_analysis/urls.py
+++ audio_analysis/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('audio/', include('audio_transcription.urls')),
     path('admin/', admin.site.urls),
     path('', include('audio_transcription.urls'))
 ]
